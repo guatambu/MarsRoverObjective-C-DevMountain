fb5c555280cf745dda0b4a681bcefab41f6ecec3
guatambu <guatambu@dundak.com>
2018-06-30 19:11:18 -0700

    Initial Commit

---
+++ RoverObjective-C-DevMountain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RoverObjective-C-DevMountain
+//
+//  Created by Kelly Johnson on 6/30/18.
+//  Copyright © 2018 Leme Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
