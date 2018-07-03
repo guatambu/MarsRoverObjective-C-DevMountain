1af2ae0b61c2fae15a10069216dd5e354a7c6c9b
guatambu <guatambu@dundak.com>
2018-07-03 11:01:08 -0700

    setup initial methods skeletons in roverClient.m

---
+++ RoverObjective-C-DevMountain/Controllers/Model Controllers/MGDMarsRoverClient.h
@@ -0,0 +1,64 @@
+//
+//  MGDMarsRoverClient.h
+//  RoverObjective-C-DevMountain
+//
+//  Created by Kelly Johnson on 7/3/18.
+//  Copyright © 2018 Leme Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MGDRover.h"
+#import "MGDRoverPhoto.h"
+
+@interface MGDMarsRoverClient : NSObject
+
+#pragma GET networking methods
+
+- (void) fetchAllMarsRoversWithCompletion:(void(^)(NSArray<NSString *> *roverNames, NSError *error))completion;
+
+- (void) fetchMissionManifestForRoverNamed:(NSString *)roverNamed completion:(void(^)(MGDRover *rover, NSError *error))completion;
+
+- (void) fetchPhotosFromRover:(MGDRover *)rover martianSol:(NSNumber *)martianSol completion:(void(^)(NSArray<UIImage *> *roverPhotos, NSError *error))completion;
+
+- (void) fetchImageDataForPhoto:(MGDRoverPhoto *)photo completion:(void(^)(NSData *data, NSError *error))completion;
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RoverObjective-C-DevMountain/Controllers/Model Controllers/MGDMarsRoverClient.m
@@ -0,0 +1,82 @@
+//
+//  MGDMarsRoverClient.m
+//  RoverObjective-C-DevMountain
+//
+//  Created by Kelly Johnson on 7/3/18.
+//  Copyright © 2018 Leme Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MGDMarsRoverClient.h"
+#import "MGDRover.h"
+#import "MGDRoverPhoto.h"
+
+@implementation MGDMarsRoverClient
+
+- (void)fetchAllMarsRoversWithCompletion:(void (^)(NSArray<NSString *> *, NSError *))completion
+{
+    
+    
+}
+
+- (void)fetchMissionManifestForRoverNamed:(NSString *)roverNamed completion:(void (^)(MGDRover *, NSError *))completion
+{
+    
+    
+}
+
+- (void)fetchPhotosFromRover:(MGDRover *)rover martianSol:(NSNumber *)martianSol completion:(void (^)(NSArray<UIImage *> *, NSError *))completion
+{
+    
+    
+}
+
+- (void)fetchImageDataForPhoto:(MGDRoverPhoto *)photo completion:(void (^)(NSData *, NSError *))completion
+{
+    
+    
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
